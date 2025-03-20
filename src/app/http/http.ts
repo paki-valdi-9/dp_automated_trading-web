930b665316493bf8273cafcb42657e9a3e57f0ac
paki-valdi-9 <palkourbanek45@gmail.com>
2025-03-20 18:34:44 +0100

    feat: backend url

--- src/app/http/http.ts
+++ src/app/http/http.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const http = axios.create({
-  baseURL: process.env.REACT_APP_BFF_API || "https://dp-automated-trading-web-api.onrender.com/api",
+  baseURL:
+    process.env.REACT_APP_BFF_API || "https://dp-automated-trading-web-public-api.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
